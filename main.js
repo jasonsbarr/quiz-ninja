bb564608748b64f20b7f7a22e2b7b28688a69999
Jason Barr <jason@jasonsbarr.com>
2019-02-20 16:02:09 -0600

    Remove prompts and use form to answer questions, complete chapter 8

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const view = {
     result: document.getElementById('result'),
     info: document.getElementById('info'),
     start: document.getElementById('start'),
+    response: document.querySelector('#response'),
 
     show(element) {
         element.style.display = 'block';
@@ -27,48 +28,69 @@ const view = {
 
         target.innerHTML = content;
     },
+
+    setup() {
+        this.show(this.question);
+        this.show(this.response);
+        this.show(this.result);
+        this.hide(this.start);
+        this.render(this.score, game.score);
+        this.render(this.result, '');
+        this.render(this.info, '');
+        this.resetForm();
+    },
+
+    resetForm() {
+        this.response.answer.value = '';
+        this.response.answer.focus();
+    },
+
+    teardown() {
+        this.hide(this.question);
+        this.hide(this.response);
+        this.show(this.start);
+    },
 };
 
 const game = {
     start(quiz) {
-        this.questions = [...quiz];
         this.score = 0;
-        view.hide(view.start);
-
-        // Main game loop
-        for (const question of this.questions) {
-            this.question = question;
-            this.ask();
-        }
-
-        this.gameOver();
+        this.questions = [...quiz];
+        view.setup();
+        this.ask();
     },
 
     ask() {
-        const question = `What is ${this.question.name}'s real name?`;
-        view.render(view.question, question);
-        const response = prompt(question);
-        this.check(response);
+        if (this.questions.length > 0) {
+            this.question = this.questions.pop();
+            const question = `What is ${this.question.name}'s real name?`;
+            view.render(view.question, question);
+        } else {
+            this.gameOver();
+        }
     },
 
-    check(response) {
+    check(event) {
+        event.preventDefault();
+        const response = view.response.answer.value;
         const answer = this.question.realName;
         if (response === answer) {
             view.render(view.result, 'Correct!', {class: 'correct'});
-            alert('Correct!');
             this.score++;
             view.render(view.score, this.score);
         } else {
-            view.render(view.result, `Wrong! The correct answer was ${answer}`);
-            alert(`Wrong! The correct answer was ${answer}.`);
+            view.render(view.result, `Wrong! The correct answer was ${answer}.`, {class: 'wrong'});
         }
+        view.resetForm();
+        this.ask();
     },
 
     gameOver() {
         view.render(view.info, `Game Over. You scored ${this.score} point${this.score !== 1 ? 's' : ''}.`);
-
-        view.show(view.start);
+        view.teardown();
     },
 }
 
-view.start.addEventListener('click', () => game.start(quiz), false)
+view.start.addEventListener('click', () => game.start(quiz), false);
+view.response.addEventListener('submit', event => game.check(event));
+view.hide(view.response);
